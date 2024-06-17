f63b007d45729f862050d745d4825eb2ca72c625
rm0087 <michetti.ray@gmail.com>
2024-06-17 15:33:06 -0400

    first class functions lab in progress

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(array){
+    let arrayLength = array.length;
+    return array.slice(arrayLength-2,arrayLength)
+}
+
+let selectingDrivers = []
+selectingDrivers.push(returnFirstTwoDrivers,returnLastTwoDrivers);
+
+function createFareMultiplier(mult){
+    return function(){}
+}
+
